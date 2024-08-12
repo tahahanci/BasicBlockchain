285ddf04a805beea9d5458d4956fcf04b577f4f8
Taha Hancı <taha.hanci@boun.edu.tr>
2024-08-12 14:19:48 +0300

    Printing blockchains using GSON library.

--- src/main/java/blockchain/BasicBlockchain.java
+++ src/main/java/blockchain/BasicBlockchain.java
@@ -1,15 +1,23 @@
 package blockchain;
 
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
 public class BasicBlockchain {
+
+    public static ArrayList<Block> blockchain = new ArrayList<>();
 
     public static void main(String[] args) {
         Block genesisBlock = new Block("This is first block", "0");
-        System.out.println("Genesis block hash: " + genesisBlock.calculateHash());
-
         Block secondBlock = new Block("This is second block", genesisBlock.hash);
-        System.out.println("Second block hash: " + secondBlock.hash);
-
         Block thirdBlock = new Block("This is third block", secondBlock.hash);
-        System.out.println("Third block hash: " + thirdBlock.hash);
+
+        blockchain.add(genesisBlock);
+        blockchain.add(secondBlock);
+        blockchain.add(thirdBlock);
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
     }
 }
