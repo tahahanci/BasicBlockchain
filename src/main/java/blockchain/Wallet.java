c3228b07a5179a5ee8481cb6443eb52ea82b837a
Taha Hancı <taha.hanci@boun.edu.tr>
2024-08-14 14:14:41 +0300

    send funds and get balance method added.

--- src/main/java/blockchain/Wallet.java
+++ src/main/java/blockchain/Wallet.java
@@ -2,11 +2,16 @@ package blockchain;
 
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Wallet {
 
     public PrivateKey privateKey;
     public PublicKey publicKey;
+
+    public HashMap<String, TransactionOutput> UTXOs = new HashMap<String, TransactionOutput>();
 
     public Wallet() {
         generateKeyPair();
@@ -26,5 +31,46 @@ public class Wallet {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public float getBalance() {
+        float total = 0;
+
+        for (Map.Entry<String, TransactionOutput> item : BasicBlockchain.UTXOs.entrySet()) {
+            TransactionOutput UTXO = item.getValue();
+            if (UTXO.isMine(publicKey)) {
+                UTXOs.put(UTXO.ID, UTXO);
+                total += UTXO.value;
+            }
+        }
+        return total;
+    }
+
+    public Transaction sendFunds(PublicKey recipient, float value) {
+        if (getBalance() < value) {
+            System.out.println("Not enough fund for transaction.");
+            return null;
+        }
+
+        ArrayList<TransactionInput> transactionInputs = new ArrayList<>();
+        float total = 0;
+
+        for (Map.Entry<String, TransactionOutput> items : UTXOs.entrySet()) {
+            TransactionOutput UTXO = items.getValue();
+            total += UTXO.value;
+            transactionInputs.add(new TransactionInput(UTXO.ID));
+
+            if (total > value)
+                break;
+        }
+
+        Transaction transaction = new Transaction(publicKey, recipient, value, transactionInputs);
+        transaction.generateSignature(privateKey);
+
+        for (TransactionInput input : transactionInputs) {
+            UTXOs.remove(input.transactionOutputID);
+        }
+
+        return transaction;
     }
 }
